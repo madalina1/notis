4f271e8726ab7aaf6512db06156963ed496e5f66
madalina1 <ioanainsuratelu@yahoo.com>
2020-01-27 21:59:10 +0200

    Added some filters

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,18 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Switch } from 'react-router-dom';
 import { routes } from './modules/routes/routes';
 import { RouteWithSubRoutes } from './modules/routes/RouteConfig';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import './assets/styles/_base.scss';
-import { Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
+import { Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, Typography, Checkbox, FormControlLabel } from '@material-ui/core';
 import { FaEnvelopeOpenText } from 'react-icons/fa';
 import { MdGTranslate } from "react-icons/md";
-import StyledTreeItem from './shared/StyledTreeItem/StyledTreeItem';
-import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 import { FiFilter } from "react-icons/fi";
 import logo from './assets/images/icon.png';
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -37,6 +35,21 @@ const useStyles = makeStyles((theme: Theme) =>
 const App: React.FC = () => {
 	const classes = useStyles();
 
+	const [state, setState] = React.useState({
+		foreignCitizen: false,
+		data: null
+	});
+
+	const handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+		setState({ ...state, [name]: event.target.checked });
+	};
+
+	const fetchNotaries = () => (event: any) => {
+		fetch('https://jsonplaceholder.typicode.com/todos/1')
+			.then(response => response.json())
+			.then((data: any) => setState({ ...state, data }));
+	}
+
 	return (
 		<div className={classes.root}>
 			<Drawer
@@ -47,24 +60,44 @@ const App: React.FC = () => {
 				}}
 				anchor="left"
 			>
-				<img src={logo} width="150px" height="auto" alt="icon" className="logo"/>
+				<img src={logo} width="150px" height="auto" alt="icon" className="logo" />
 				<Divider />
 				<List className="menu-container">
-					{['Filter by', 'Price'].map((text, index) => (
-						<ListItem key={text} className="menu-element">
-							{index === 0 && <ListItemIcon className="menu-icon">{<FiFilter />}</ListItemIcon>}
-							<ListItemText primary={index===0 ? <Typography style={{ fontWeight: 'bold' }}>{text}</Typography> : text} className={index === 0 ? 'menu-filter' : 'menu-filter-element'}/>
-						</ListItem>
-					))}
+					<ListItem key="Filter by" className="menu-element">
+						<ListItemIcon className="menu-icon">{<FiFilter />}</ListItemIcon>
+						<ListItemText primary={<Typography style={{ fontWeight: 'bold' }}>Filter by</Typography>} className='menu-filter' />
+					</ListItem>
+					<ListItem key="Price" className="menu-element">
+						<ListItemText primary={<div style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center' }}><div>Price</div>
+
+						</div>} className='menu-filter-element' />
+					</ListItem>
+					<ListItem key="Foreign Citizen" className="menu-element">
+						<ListItemText primary={<div style={{ display: "flex", justifyContent: 'space-between', alignItems: 'center' }}><div>Foreign Citizen</div>
+							<FormControlLabel
+								control={
+									<Checkbox
+										checked={state.foreignCitizen}
+										onChange={handleChange('foreignCitizen')}
+										value="foreignCitizen"
+										color="primary"
+									/>
+								}
+								label=""
+							/>
+						</div>} className='menu-filter-element' />
+					</ListItem>
 				</List>
 				<Divider />
 				<List>
-					{['See all notaries', 'See all translators'].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon className="menu-icon">{index === 0 ? <FaEnvelopeOpenText /> : <MdGTranslate />}</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
+					<ListItem button key='See all notaries'>
+						<ListItemIcon className="menu-icon">{<FaEnvelopeOpenText />}</ListItemIcon>
+						<ListItemText primary='See all notaries' onClick={fetchNotaries()} />
+					</ListItem>
+					<ListItem button key='See all translators'>
+						<ListItemIcon className="menu-icon">{<MdGTranslate />}</ListItemIcon>
+						<ListItemText primary='See all translators' />
+					</ListItem>
 				</List>
 			</Drawer>
 			<Switch>
